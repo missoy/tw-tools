5cd7cf461367ba8bd1ce6d6bc36a9c53741ab461
ruby <916809498@qq.com>
2019-06-13 20:12:53 +0800

    Add related branch when switch branch.

--- src/main/java/com/baiguiren/tools/branch/SwitchBranch.java
+++ src/main/java/com/baiguiren/tools/branch/SwitchBranch.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.project.ProjectManager;
 import git4idea.branch.GitBrancher;
 import git4idea.repo.GitRepository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +18,7 @@ import java.util.stream.Collectors;
 public class SwitchBranch {
     public static void switchTo(String to) {
         Project project = ProjectManager.getInstance().getOpenProjects()[0];
+        GitBrancher gitBrancher = GitBrancher.getInstance(project);
         Collection<Repository> repositories = VcsRepositoryManager.getInstance(project).getRepositories();
 
         List<GitRepository> gitRepositories = repositories
@@ -24,7 +26,18 @@ public class SwitchBranch {
                                                 .map(obj -> (GitRepository) obj)
                                                 .collect(Collectors.toList());
 
-        GitBrancher gitBrancher = GitBrancher.getInstance(project);
+        // 本地没有那个分支的时候创建, 同时需要远程分支也存在(否则会报错)
+        List<GitRepository> forCheckoutRepositories = new ArrayList<>();
+        for (GitRepository gitRepository : gitRepositories) {
+            if (gitRepository.getBranches().findLocalBranch("refs/heads/" + to) == null) {
+                if (gitRepository.getBranches().getRemoteBranches().stream().anyMatch(gitRemoteBranch -> gitRemoteBranch.toString().equals("refs/remotes/origin/" + to))) {
+                    forCheckoutRepositories.add(gitRepository);
+                }
+            }
+        }
+        if (!forCheckoutRepositories.isEmpty())
+            gitBrancher.checkoutNewBranchStartingFrom(to, "refs/remotes/origin/" + to, forCheckoutRepositories, () -> {});
+
         gitBrancher.checkout(to, false, gitRepositories, () -> System.out.println("Finish checkout."));
 
         Notification notification = new Notification("tw tools", "Switch Branch", "Successful switch to " + to + ".", NotificationType.INFORMATION);
