76825149c504766b3d3794fb49bfb33e313484fc
ruby <916809498@qq.com>
2019-10-22 10:07:34 +0800

    new version

--- src/main/java/com/baiguiren/tools/branch/V128.java
+++ src/main/java/com/baiguiren/tools/branch/V128.java
@@ -4,9 +4,9 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class V126 extends AnAction {
+public class V128 extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        SwitchBranch.switchTo("dev/v1.2.6");
+        SwitchBranch.switchTo("dev/v1.2.8");
     }
 }
