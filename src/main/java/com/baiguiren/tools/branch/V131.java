bc4353b8cab81dc46c54c5f2572a997c32ef8bc0
ruby <916809498@qq.com>
2020-02-04 11:36:28 +0800

    new version support

--- src/main/java/com/baiguiren/tools/branch/V131.java
+++ src/main/java/com/baiguiren/tools/branch/V131.java
@@ -4,9 +4,9 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class V1281 extends AnAction {
+public class V131 extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        SwitchBranch.switchTo("dev/v1.2.8.1");
+        SwitchBranch.switchTo("dev/v1.3.1");
     }
 }
