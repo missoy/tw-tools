a3b21e69474fad408e38d43585144b57d1122f5f
ruby <916809498@qq.com>
2020-03-10 11:44:22 +0800

    mod 移除无效文件，根据当前 .env 跳转

--- src/main/java/com/baiguiren/tools/docs/DocsAction.java
+++ src/main/java/com/baiguiren/tools/docs/DocsAction.java
@@ -1,5 +1,6 @@
 package com.baiguiren.tools.docs;
 
+import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.CaretModel;
@@ -7,24 +8,67 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DocsAction {
-    public static void openInBrowser(String env, AnActionEvent e) {
-        String docsHost = env + ".api.86yqy.com";
+public class DocsAction extends AnAction {
+    private static AnActionEvent anActionEvent;
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        openInBrowser(e);
+    }
+
+    /**
+     * 在浏览器打开文档链接
+     *
+     * @param e AnActionEvent
+     */
+    private static void openInBrowser(AnActionEvent e) {
+        anActionEvent = e;
         String line = getLineContent(e);
         String urlStr = matchUrl(line);
         if (urlStr.equals("")) {
             return;
         }
 
-        urlStr = urlStr.replace("qa1.api.86yqy.com", docsHost);
+        urlStr = urlStr.replace("qa1.api.86yqy.com", getEnvDomain());
         open(urlStr);
     }
 
+    /**
+     * @return 获取 .env 配置的 url 对应的域名
+     */
+    private static String getEnvDomain() {
+        String basePath = anActionEvent.getProject().getBasePath();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(basePath + "/.env"));
+            for (String line: lines) {
+                if (StringUtil.startsWith(line, "url=")) {
+                    line = line.replace("url=", "");
+                    line = line.trim();
+                    line = line.replace("http://", "");
+                    return line;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    /**
+     * @param e AnActionEvent
+     * @return 光标所在行的内容
+     */
     private static String getLineContent(AnActionEvent e) {
         final Editor editor = e.getData(CommonDataKeys.EDITOR);
         if (editor == null) return "";
@@ -39,6 +83,12 @@ public class DocsAction {
         return document.getText(new TextRange(lineStartOffset, lineEndOffset));
     }
 
+    /**
+     * 匹配光标所在行的 url
+     *
+     * @param line 光标所在行
+     * @return url
+     */
     private static String matchUrl(String line) {
         String pattern = "[\\s\\S]*(http://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])[\\s\\S]*";
         Pattern p = Pattern.compile(pattern);
@@ -51,6 +101,11 @@ public class DocsAction {
         return "";
     }
 
+    /**
+     * 在浏览器中打开对应的 url
+     *
+     * @param url 需要打开的 url
+     */
     private static void open(String url) {
         Runtime rt = Runtime.getRuntime();
         String os = StringUtil.toLowerCase(System.getProperty("os.name"));
